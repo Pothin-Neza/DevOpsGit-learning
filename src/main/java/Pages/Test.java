b3a9906bac7c564cd67d9a15e0bde9f794704118
Pothin <pothin.neza2023@kepler.org>
2026-02-26 14:55:51 +0200

    feat(main): simulate team changes on Test.java

---
+++ src/main/java/Pages/Test.java
@@ -0,0 +1,8 @@
+package Pages;
+
+public class Test {
+    public static void main(String[] args) {
+        // Change from main branch
+        System.out.println("Update from main branch");
+    }
+}
